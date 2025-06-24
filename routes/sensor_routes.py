9dfa187321be8411a497cb4d413d06573855f01d
christennguyen1 <96573270+christennguyen1@users.noreply.github.com>
2025-06-24 15:00:21 +0700

    Update sensor_routes.py

--- routes/sensor_routes.py
+++ routes/sensor_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Request
 from fastapi.security import OAuth2PasswordBearer
-from controller.sensors_controller import  controller_post_data
+from controller.sensors_controller import controller_post_data
 
 router = APIRouter()
 
@@ -9,3 +9,5 @@ router = APIRouter()
 async def create_sensor_data(request: Request):
     body = await request.json()
     return await controller_post_data(body)
+
+
